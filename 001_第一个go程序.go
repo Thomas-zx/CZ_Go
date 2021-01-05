cab0e8555b1e7b5c2c73a7c20dd21b98170d52bd
张兴 <zhangxingmsn@gmail.com>
2021-01-05 18:13:57 +0800

    002.变量定义和使用

--- 001_第一个go程序.go
+++ 001_第一个go程序.go
@@ -1,5 +1,5 @@
 //文件所属的包 在go语言中 主函数所在的包一定是main
-package main
+package main0001
 
 //导入系统包 标准输入输出包
 import "fmt"
@@ -7,7 +7,7 @@ import "fmt"
 //func 函数格式 main 函数名 主函数
 //程序有且只有一个主函数  无论程序中有多少函数 都会从main进入
 //() 函数参数列表 {}函数体 代码体 程序体
-func main()  {
+func main001()  {
 	fmt.Println("hello world")
 }
 // 注释 单行注释
